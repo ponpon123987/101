0b656f292012af8836de3647fd4fae01e5eff80e
ponpon123987 <ponpon123987@gmail.com>
2019-08-26 17:23:45 +0800

    0826

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
+const { Pool } = require("pg");
 
 var request = require("request");
 
@@ -35,6 +36,23 @@ services:
     res.send(example_manifest_code);
 });
 
+app.post("/manifestrm",(req,res)=>{
+
+  var example_manifest_code = `
+---
+applications:
+- name: ${req.body.app_Name}
+  memory: ${req.body.application_memory}M
+  disk_quota: ${req.body.application_diskqouta}M
+  buildpack: nodejs_buildpack
+  command: node app.js
+services:
+- ${req.body.service_instance}
+- ${req.body.serviceInstanceMongoDB}`;
+  
+  res.send(example_manifest_code);
+});
+
 app.post("/deploy",(req,res)=>{
 
   var deploy_guide = `
@@ -50,12 +68,29 @@ app.post("/dbCheck",(req,res)=>{
   const port = req.body.port;
   const database = req.body.database;
 
-  const dbUri = `mongodb://${username}:${password}@${external}:${port}/${database}`
-  mongoose.connect(dbUri, { useNewUrlParser: true })
-  .then(() => res.send('success'))
-  .catch(err => res.send(err))
+  const dbUri = `mongodb://${username}:${password}@${external}:${port}/${database}`;
+  const pool = new Pool({
+    user: username,
+    host: external,
+    database: database,
+    password: password,
+    port: port
+  })
+  
+  if(req.body.type=='mongodb'){
+    mongoose.connect(dbUri, { useNewUrlParser: true })
+      .then(() => res.send('success'))
+      .catch(err => res.send(err))
 
-  mongoose.connection.close();
+      mongoose.connection.close();
+  }else if(req.body.type=='postgres'){
+    pool.connect()
+      .then(() => res.send('success'))
+      .catch(err => res.send(err))
+
+      pool.end();
+  }
+
 });
 
 app.post("/indexFile", (req, res) => {
@@ -63,70 +98,193 @@ app.post("/indexFile", (req, res) => {
   if (req.body.type === "rabbitmq") {
     
   var example_node_code = `
-    //Node Modules to install//
-    const express = require('express');
-    const mqtt = require('mqtt');
+//Node Modules to install//
+const express = require('express');
+const mqtt = require('mqtt');
 
-    app = express();
+app = express();
 
-    app.use('/',(req,res)=>{
-        res.send('hello world')
-    })
+app.use('/',(req,res)=>{
+    res.send('hello world')
+})
 
-    //Get the WISE-PaaS environment vcap_services config 
-    let vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+//Get the WISE-PaaS environment vcap_services config 
+let vcap_services = JSON.parse(process.env.VCAP_SERVICES);
 
-    //Set up connection parameters to RabbitMQ
-    mqtt_servicename="${req.body.serviceName}"
-    mqtt_broker = "mqtt://"+ vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.host;
-    mqtt_username = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.username.trim();
-    mqtt_password = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.password.trim();
-    mqtt_port =vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.port;
+//Set up connection parameters to RabbitMQ
+mqtt_servicename="${req.body.serviceName}"
+mqtt_broker = "mqtt://"+ vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.host;
+mqtt_username = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.username.trim();
+mqtt_password = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.password.trim();
+mqtt_port =vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.port;
     
-    mqtt_options=
-    {
-        broker:mqtt_broker,
-        reconnectPeriod: 1000,
-        port: mqtt_port,
-        username: mqtt_username,
-        password: mqtt_password
-    };
+mqtt_options=
+{
+    broker:mqtt_broker,
+    reconnectPeriod: 1000,
+    port: mqtt_port,
+    username: mqtt_username,
+    password: mqtt_password
+};
 
-    mqtt_topic = "/${req.body.topic}/#";
-    mqtt_retain = true;
+mqtt_topic = "/${req.body.topic}/#";
+mqtt_retain = true;
 
-    //Set up connection to RabbitMQ
-    var client = mqtt.connect(mqtt_broker,mqtt_options);
+//Set up connection to RabbitMQ
+var client = mqtt.connect(mqtt_broker,mqtt_options);
 
-    client.on("connect", function() {
-      client.subscribe(mqtt_topic);
-      console.log("[MQTT]:", "Connected.");
-    });
+client.on("connect", function() {
+    client.subscribe(mqtt_topic);
+    console.log("[MQTT]:", "Connected.");
+});
     
-    client.on("message", function(topic, message) {
-      console.log("[" + topic + "]:" + message.toString());
-    });
+client.on("message", function(topic, message) {
+    console.log("[" + topic + "]:" + message.toString());
+});
     
-    client.on("error", function(err) {
-      console.log(err);
-    });
+client.on("error", function(err) {
+    console.log(err);
+});
     
-    client.on("close", function() {
-      console.log("[MQTT]: close");
-    });
+client.on("close", function() {
+    console.log("[MQTT]: close");
+});
     
-    client.on("offline", function() {
-      console.log("[MQTT]: offline");
-    });
+client.on("offline", function() {
+    console.log("[MQTT]: offline");
+});
 
-    const port = process.env.PORT || 3000
-    app.listen(port,()=>{
-        console.log(\`listen port on process \${port} \`);
-    })`;
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(\`listen port on process \${port} \`);
+})`;
     res.send(example_node_code);
   } else {
     res.send("no type");
   }
+});
+
+app.post("/indexFilerm",(req,res)=>{
+  // console.log(req.body)
+ 
+  var example_node_code = `
+//Node Modules to install//
+//npm init -y
+//npm install express mqtt mongoose --save
+
+const express = require('express');
+const mqtt = require('mqtt');
+const mongoose = require("mongoose");
+
+app = express();
+
+app.use('/',(req,res)=>{
+    res.send('hello world')
+})
+
+app.get("/query", (req, res) => {
+  ${req.body.SchemaName}.find({}).then((err, data) => {
+    if (err) res.send(err);
+    else {
+      res.json(data);
+    }
+  });
+});
+
+//Get the WISE-PaaS environment vcap_services config 
+let vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+
+    
+mongodb_service_name = "${req.body.serviceNameMongoDB}";
+
+let replicaSetName =
+  vcap_services[mongodb_service_name][0].credentials.replicaSetName;
+let db =
+  vcap_services[mongodb_service_name][0].credentials.uri +
+    "?replicaSet=" +replicaSetName;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to the MongoDB..."))
+  .catch(err => console.log("Could not connect to MongoDB...", err));
+
+//Schema;
+const Schem = new mongoose.Schema(
+{
+  date: Date,
+  topic: String,
+  data: Number
+},
+  { versionKey: false }
+);
+
+const ${req.body.SchemaName} = mongoose.model("${req.body.SchemaName}" ,Schem);
+
+
+//Set up connection parameters to RabbitMQ
+mqtt_servicename="${req.body.serviceName}"
+mqtt_broker = "mqtt://"+ vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.host;
+mqtt_username = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.username.trim();
+mqtt_password = vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.password.trim();
+mqtt_port =vcap_services[mqtt_servicename][0].credentials.protocols.mqtt.port;
+    
+mqtt_options=
+{
+    broker:mqtt_broker,
+    reconnectPeriod: 1000,
+    port: mqtt_port,
+    username: mqtt_username,
+    password: mqtt_password
+};
+
+mqtt_topic = "/${req.body.topic}/#";
+mqtt_retain = true;
+
+//Set up connection to RabbitMQ
+var client = mqtt.connect(mqtt_broker,mqtt_options);
+
+client.on("connect", function() {
+  client.subscribe(mqtt_topic);
+  console.log("[MQTT]:", "Connected.");
+});
+    
+client.on("message", function(topic, message) {
+  console.log("[" + topic + "]:" + message.toString());
+    
+  var d = new Date();
+  var n = d.getTime();
+  console.log("n", n);
+  console.log("topic", topic);
+  console.log("data", message.toString());
+  const ${req.body.SchemaName} = new ${req.body.SchemaName}({
+    date: n,
+    topic: topic,
+    data: message
+  });
+    
+  ${req.body.SchemaName}.save();
+});
+    
+client.on("error", function(err) {
+  console.log(err);
+});
+    
+client.on("close", function() {
+  console.log("[MQTT]: close");
+});
+    
+client.on("offline", function() {
+  console.log("[MQTT]: offline");
+});
+
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    console.log(\`listen port on process \${port} \`);
+})`;
+res.send(example_node_code);
+
+
+
 });
 
 app.post("/userId",(req,res)=>{
